d5945942c70c35616cbb968c08bd35cd492823f1
Ismael Guedes <113357254+ismaelvguedes@users.noreply.github.com>
2024-08-15 15:30:03 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'dephino.com', 'www.dephino.com']
+ALLOWED_HOSTS = ['.vercel.app', 'zssn.dephino.com']
 
 
 # Application definition
